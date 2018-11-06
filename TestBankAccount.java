8570156e84b97f0ec6826143696e9b2083872972
rmmr1002 <rmmr1002@gmail.com>
2018-11-05 19:32:31 -0500

    Update TestBankAccount.java

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -5,5 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
+  
+  @Test
+  public void testDebit(){
+    BankAccount account=new Account(10);
+    double amount=account.debit(5);
+    Assert.assertEquals(5.0,amount);
+  }
+    
 
 }
